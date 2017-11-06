7b0113793dbea856c12550b28d167c8d2bdd9793
Jayanth7778 <gundalajayanthkumargoud@gmail.com>
2017-11-06 16:07:38 +0530

    backend

---
+++ E_CommerceHoneyBackend/src/main/java/com/niit/daoimpl/UserDAOImpl.java
@@ -0,0 +1,108 @@
+package com.niit.daoimpl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.dao.UserDAO;
+import com.niit.model.User;
+
+@Repository("userDAO")
+@Transactional
+public class UserDAOImpl implements UserDAO
+{
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+		
+	public UserDAOImpl(SessionFactory sessionFactory)
+	{
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Transactional
+	public boolean save(User user)
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().save(user);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	@Transactional
+	public boolean update(User user)
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().update(user);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	@Transactional
+	public boolean validate(String id, String password)
+	{
+		Query query=sessionFactory.getCurrentSession().createQuery(" from User where id = ? and password = ?");
+		query.setString(0, id);    
+		query.setString(1, password);
+	
+	 if(  query.uniqueResult()  == null)
+	 {
+		 return false;
+	 }
+	 else
+	 {
+		 return true;
+	 }
+	
+	}
+
+	/*@SuppressWarnings("unchecked")
+	@Override
+	@Transactional
+	public List<User> retrieveUser()
+	{
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("from User");
+		List<User> listUser=query.list();
+		session.close();
+		return listUser;
+	} 
+*/
+	@Transactional
+	public User get(String id)
+	{
+	  return 	(User)  sessionFactory.getCurrentSession().get(User.class, id);
+	
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> list() 
+	{
+
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("from User");
+		List<User> listUser=query.list();
+		session.close();
+		return listUser;
+	}
+
+}
---
+++ E_CommerceHoneyBackend/src/test/java/com/niit/test/UserDAOTestCase.java
@@ -0,0 +1,89 @@
+package com.niit.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.config.DBConfig;
+import com.niit.dao.UserDAO;
+import com.niit.model.User;
+
+public class UserDAOTestCase
+{
+
+	@Autowired
+	static DBConfig context;
+
+	@Autowired
+	static UserDAO userDAO;
+
+	@Autowired
+	static User user;
+
+	@SuppressWarnings("resource")
+	@BeforeClass
+	public static void initialize()
+	{
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.niit");
+		context.refresh();
+
+		userDAO = (UserDAO) context.getBean("userDAO");
+
+		user = (User) context.getBean("user");
+
+	}
+
+	@Ignore
+ 	@Test
+	public void createUserTestCase()
+	 {
+		user.setId("Sainath");
+		user.setName("Sainath");
+		user.setPassword("Sainath");
+		user.setRole("ROLE_USER");
+		user.setContact("8885288124");
+		boolean flag = userDAO.save(user);
+
+		assertEquals("createUserTestCase", true, flag);
+
+	}
+
+	@Ignore
+	@Test
+	public void updateUserTestCase()
+	{
+		user.setId("Jayanth");
+		user.setName("Jayanth");
+		user.setPassword("Jayanth");
+		user.setRole("ROLE_USER");
+		user.setContact("9676963891");
+		boolean flag = userDAO.update(user);
+
+		assertEquals("updateUserTestCase", true, flag);
+
+	}
+
+	@Ignore
+	@Test
+	public void validateUserTestCase()
+	 {
+
+		boolean flag = userDAO.validate("Sainath", "Sainath");
+		assertEquals(true, flag);
+
+	}
+
+	@Ignore
+	@Test
+	public void listAllUserTestCase()
+	 {
+		int actualSize = userDAO.list().size();
+		assertEquals(3, actualSize);
+	}
+
+}
